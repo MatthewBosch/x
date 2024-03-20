99baf2bdff8bdbbef2ce1de306d4284938c7832d
MatthewBosch <152050466+MatthewBosch@users.noreply.github.com>
2024-03-21 00:49:25 +0800

    Update subspace.sh

--- subspace.sh
+++ subspace.sh
@@ -1,5 +1,5 @@
 cd /home
-curl -s https://github.com/MatthewBosch/x/blob/main/docker-compose.yaml -o docker-compose.yml
+curl -s https://github.com/MatthewBosch/x/blob/main/docker-compose.yaml -o docker-compose.yaml
 iptables -P INPUT ACCEPT   
 iptables -P OUTPUT ACCEPT 
 docker compose up -d 
