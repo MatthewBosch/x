a8680063e1248cd4d5dd604fd68c367cc33a6575
MatthewBosch <152050466+MatthewBosch@users.noreply.github.com>
2024-03-21 00:45:57 +0800

    Update and rename sp.sh to subspace-nodocker.sh

---
+++ subspace-nodocker.sh
@@ -0,0 +1,6 @@
+curl -s https://github.com/MatthewBosch/x/blob/main/docker-compose.yaml -o docker-compose.yml
+iptables -P INPUT ACCEPT   
+iptables -P OUTPUT ACCEPT 
+wget -qO- https://get.docker.com/ | sh
+docker compose up -d 
+docker compose logs --tail=1000 -f
