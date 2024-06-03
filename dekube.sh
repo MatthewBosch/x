c6bd796ff81aa941f996ae3d9227ac206f506bf1
MatthewBosch <152050466+MatthewBosch@users.noreply.github.com>
2024-06-04 01:59:23 +0800

    Create dekube.sh

---
+++ dekube.sh
@@ -0,0 +1,4 @@
+bash <(curl -sLk https://s.dekube.ai/dekube+client/scripts/DEKUBE_1.0.3.sh)
+dekube register Mzg4MjBmOGMtN2EzOC00ZWM0-26a+VxClEPwl75wT4aPPMqTR3uAma6dm65W1oUk
+cd dekube-client
+nohup ./start_dekube.sh
