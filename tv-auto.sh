93319886c89c96104982ad5c39b2a3bf01d412d9
MatthewBosch <152050466+MatthewBosch@users.noreply.github.com>
2024-06-28 19:08:34 +0800

    Update tv-auto.sh

--- tv-auto.sh
+++ tv-auto.sh
@@ -9,7 +9,7 @@ titan1: &base_config
     container_name: titan1
     environment:
       AppConfig__TITAN_NETWORK_LOCATORURL: "https://cassini-locator.titannet.io:5000/rpc/v0"
-      AppConfig__TITAN_STORAGE_STORAGEGB: "20"
+      AppConfig__TITAN_STORAGE_STORAGEGB: "22"
       AppConfig__TITAN_STORAGE_PATH: ""
       AppConfig__TITAN_EDGE_BINDING_URL: "https://api-test1.container1.titannet.io/api/v2/device/binding"
       AppConfig__TITAN_EDGE_ID: "EFCF32A4-16D5-4559-B7E1-74F60FCB7D48"
