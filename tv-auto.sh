57514785a1cea96ddb01739c6ec024b14b6bb7d8
MatthewBosch <152050466+MatthewBosch@users.noreply.github.com>
2024-06-28 19:07:26 +0800

    Update tv-auto.sh

--- tv-auto.sh
+++ tv-auto.sh
@@ -4,13 +4,12 @@
 cat <<EOF > docker-compose.yml
 version: '3.0'
 services:
-
-  titan1: 
+titan1: &base_config
     image: aron666/aron-titan-edge
     container_name: titan1
     environment:
-      AppConfig__TITAN_NETWORK_LOCATORURL: "https://test-locator.titannet.io:5000/rpc/v0"
-      AppConfig__TITAN_STORAGE_STORAGEGB: "22"
+      AppConfig__TITAN_NETWORK_LOCATORURL: "https://cassini-locator.titannet.io:5000/rpc/v0"
+      AppConfig__TITAN_STORAGE_STORAGEGB: "20"
       AppConfig__TITAN_STORAGE_PATH: ""
       AppConfig__TITAN_EDGE_BINDING_URL: "https://api-test1.container1.titannet.io/api/v2/device/binding"
       AppConfig__TITAN_EDGE_ID: "EFCF32A4-16D5-4559-B7E1-74F60FCB7D48"
