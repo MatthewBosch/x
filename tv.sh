51628a7dcecce479cd11cf31fd74ba9c10bcd7ff
MatthewBosch <152050466+MatthewBosch@users.noreply.github.com>
2024-06-30 10:36:05 +0800

    Update tv.sh

--- tv.sh
+++ tv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create docker-compose.yml file
-cat <<EOF > docker-compose.yml
+cat <<EOF > /root/docker-compose.yml
 version: '3.0'
 services:
 
@@ -9,28 +9,18 @@ services:
     image: aron666/aron-titan-edge
     container_name: titan1
     environment:
-      AppConfig__TITAN_NETWORK_LOCATORURL: "https://test-locator.titannet.io:5000/rpc/v0"
+      AppConfig__TITAN_NETWORK_LOCATORURL: "https://cassini-locator.titannet.io:5000/rpc/v0"
       AppConfig__TITAN_STORAGE_STORAGEGB: "22"
       AppConfig__TITAN_STORAGE_PATH: ""
       AppConfig__TITAN_EDGE_BINDING_URL: "https://api-test1.container1.titannet.io/api/v2/device/binding"
-      AppConfig__TITAN_EDGE_ID: "your id"
+      AppConfig__TITAN_EDGE_ID: "1E80B06C-06E1-498E-AA11-4F1C3A5DAE5F"
     restart: always
     volumes:
       - ~/.titanedge:/root/.titanedge
     ports:
-    - 1234:1234/tcp
-    - 1234:1234/udp
-
+      - "1234:1234"
+      - "1234:1234/udp"
 EOF
 
-# Prompt user to input the id code
-read -p "Please enter the id code: " id_code
-
-# Replace the placeholder in the YAML file with the provided id code
-sed -i "s/your id/$id_code/g" docker-compose.yml
-
-# Install Docker
 wget -qO- https://get.docker.com/ | sh
-
-# Run Docker Compose
 docker compose up -d
