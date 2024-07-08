8143f2b4508b99d8b44076dc82f534cc12bc23a4
MatthewBosch <152050466+MatthewBosch@users.noreply.github.com>
2024-07-08 18:29:40 +0800

    Update gaganode.sh

--- gaganode.sh
+++ gaganode.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo ufw allow 1:65000/udp
 sudo ufw allow 1:65000/tcp
-sleep 10
 sudo apt-get update -y && sudo apt-get -y install curl tar ca-certificates
 curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64
 sudo ./apphub service remove && sudo ./apphub service install
