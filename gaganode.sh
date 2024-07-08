b7d456f8239a2fe99b2798fc65aa59c6b8aa3df8
MatthewBosch <152050466+MatthewBosch@users.noreply.github.com>
2024-07-08 18:01:57 +0800

    Update gaganode.sh

--- gaganode.sh
+++ gaganode.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo ufw allow 1:65000/udp
+sudo ufw allow 1:65000/tcp
+mkdir /root/gaganode
+cd /root/gaganode
 curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64
 sudo ./apphub service remove && sudo ./apphub service install
 sudo ./apphub service start
