101877f9ad1498d139e4f1a4889728011dcb7c49
MatthewBosch <152050466+MatthewBosch@users.noreply.github.com>
2024-07-08 17:49:24 +0800

    Create gaganode.sh

---
+++ gaganode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64
+sudo ./apphub service remove && sudo ./apphub service install
+sudo ./apphub service start
+./apphub status
+sleep 10
+sudo ./apps/gaganode/gaganode config set --token=mgmxwoxomweqsoltac31d1c1adf6b598
+./apphub restart
